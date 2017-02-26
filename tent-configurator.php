c44afe7031ed7a191b7a23c486375e251cac158b
elio.schmutz <elio.schmutz@gmail.com>
2017-02-26 23:30:40 +0100

    Move data from client to server

--- tent-configurator.php
+++ tent-configurator.php
@@ -20,6 +20,56 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+class Model {
+    public function get_tents() {
+        return array(
+            array(
+                'id' => 1,
+                'title' => 'Grosses Zelt',
+                'price' => 15,
+                'length' => 4,
+                'width' => 20,
+                'minPersons' => 100,
+                'maxPersons' => 150
+                ),
+            array(
+                'id' => 2,
+                'title' => 'Grosses Zelt',
+                'price' => 18,
+                'length' => 4,
+                'width' => 20,
+                'minPersons' => 120,
+                'maxPersons' => 160
+                )
+        );
+    }
+    public function get_equipment() {
+        return array(
+        'equipment' => array(
+            array(
+                'id' => 1,
+                'title' => 'Lampe',
+                'price' => 15,
+                'description' => 'kleine Lampe für das DJ Pult',
+                ),
+            array(
+                'id' => 2,
+                'title' => 'Tisch',
+                'price' => 20,
+                'description' => 'Tisch für 6 Personen inkl. 2 Bänke',
+                )
+            )
+        );
+    }
+    public function get_data() {
+        return array(
+            'tents' => $this->get_tents(),
+            'equipment' => $this->get_equipment()
+            );
+    }
+}
+
+
 function tentconfigurator_enqueue_script() {
   wp_enqueue_script(
     'tent-configurator', plugin_dir_url( __FILE__ ) . 'dist/js/all.min.js',
@@ -37,4 +87,12 @@ add_shortcode('tent-configurator', 'tent_configurator_creation');
 
 function tent_configurator_creation() {
     return file_get_contents( plugin_dir_url( __FILE__ ) . 'dist/form.html' );
+}
+
+
+add_action( 'wp_ajax_nopriv_kah_get_data', 'kah_get_data' );
+
+function kah_get_data() {
+    $model = new Model();
+    wp_send_json($model->get_data());
 }
