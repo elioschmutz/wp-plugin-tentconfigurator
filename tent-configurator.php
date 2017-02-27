a5e7d787e2a41cbbf8bbab9da30590564d2a86f6
elio.schmutz <elio.schmutz@gmail.com>
2017-02-27 22:42:04 +0100

    Add email

--- tent-configurator.php
+++ tent-configurator.php
@@ -66,7 +66,12 @@ class Model {
             );
     }
 }
-
+function google_api() {
+    echo '<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqddQmeI9i2Ymt9kgrRPfZCio4uReQyI8&libraries=places&callback=initGoogleSearch"
+     async defer></script>';
+}
+add_action('admin_head', 'google_api');
+add_action('wp_head', 'google_api');
 
 function tentconfigurator_enqueue_script() {
   wp_enqueue_script(
@@ -87,6 +92,122 @@ function tent_configurator_creation() {
     return file_get_contents( plugin_dir_url( __FILE__ ) . 'dist/form.html' );
 }
 
+add_action( 'wp_ajax_nopriv_kah_send_offer', 'kah_send_offer' );
+
+function kah_send_offer() {
+    if ( ! json_decode($_POST['acceptTerms']) ) {
+        wp_send_json_error();
+    }
+    $action = sanitize_text_field($_POST['action']);
+    $salutation = sanitize_text_field($_POST['salutation']);
+    $firstName = sanitize_text_field($_POST['firstName']);
+    $lastName = sanitize_text_field($_POST['lastName']);
+    $company = sanitize_text_field($_POST['company']);
+    $street = sanitize_text_field($_POST['street']);
+    $streetnr = sanitize_text_field($_POST['streetnr']);
+    $zip = sanitize_text_field($_POST['zip']);
+    $location = sanitize_text_field($_POST['location']);
+    $email = sanitize_text_field($_POST['email']);
+    $phone = sanitize_text_field($_POST['phone']);
+    $comment = sanitize_text_field($_POST['comment']);
+    $acceptTerms = sanitize_text_field($_POST['acceptTerms']);
+    $eventname = sanitize_text_field($_POST['eventname']);
+    $eventdays = sanitize_text_field($_POST['eventdays']);
+    $eventlocation = sanitize_text_field($_POST['eventlocation']);
+    $eventlocationdistance = ceil(sanitize_text_field($_POST['eventlocationdistance']) / 1000);
+    $eventstartdate = date_i18n(
+        get_option( 'date_format' ),
+        strtotime(sanitize_text_field($_POST['eventstartdate'])));
+    $transportPrice = sanitize_text_field($_POST['transportPrice']);
+    $totalTentsPrice = sanitize_text_field($_POST['totalTentsPrice']);
+    $totalEquipmentPrice = sanitize_text_field($_POST['totalEquipmentPrice']);
+    $totalPrice = sanitize_text_field($_POST['totalPrice']);
+    $totalMinPersons = sanitize_text_field($_POST['totalMinPersons']);
+    $totalMaxPersons = sanitize_text_field($_POST['totalMaxPersons']);
+    $totalSquareMeters = sanitize_text_field($_POST['totalSquareMeters']);
+
+    // Handle required fields
+    if (!$salutation || !$firstName || !$lastName || !$street ||
+        !$streetnr || !$zip || !$location || !$email || !$phone ||
+        !$eventname || !$eventlocation) {
+        wp_send_json_error();
+    }
+
+    $selectedTentsString = '';
+    foreach ($_POST['selectedTents'] as $key=>$value) {
+        $index = $key + 1;
+        $selectedTentsString .= "
+Zelt {$index}
+Titel: {$value['title']}
+Preis pro Tag: {$value['price']} CHF
+Dimensionen (LxB): {$value['length']}m x {$value['width']}m
+Personen: {$value['minPersons']} - {$value['maxPersons']}
+Quadratmeter: {$value['squareMeter']}
+------------
+        ";
+    };
+    unset($value);
+
+    // error_log( print_r($_POST['selectedTents'], TRUE) );
+    // error_log($_POST['selectedTents'][0]['title']);
+    $to = sanitize_email($_POST['email']);
+    $subject = "Zeltanfrage von {$salutation} {$firstName} {$lastName}";
+    $message = "Zeltanfrage von {$salutation} {$firstName} {$lastName} erhalten!
+
+Voraussichtlicher Gesamtpreis: {$totalPrice} CHF
+
+Kommentar vom Kunden: ${comment}
+
+Kontaktdaten:
+=============
+
+Anrede: {$salutation}
+Name: {$lastName}
+Vorname: {$firstName}
+Firma: {$company}
+Strasse / Nr: {$street} {$streetnr}
+PLZ / Ort: {$zip} {$location}
+
+E-Mail: {$email}
+Telefon: {$phone}
+
+Eventdaten:
+===========
+
+Eventname: {$eventname}
+Eventort: {$eventlocation}
+Startdatum: {$eventstartdate}
+Anzahl Tage: {$eventdays}
+Distanz pro Fahrt: {$eventlocationdistance} KM
+
+Ausgewählte Zelte:
+{$selectedTentsString}
+
+Ausgewähltes Equipment
+
+Unverbindliche Preise:
+================================
+Total für {$eventdays} Tage: ${totalPrice}
+Zeltmiete pro Tag: ${totalTentsPrice}
+Equipmentmiete pro Tag: ${totalEquipmentPrice}
+
+Transportkosten hin und zurück: 
+
+Weitere Schritte:
+=================
+- Den Kunden im Bitrix erfassen: http://bitrix24.de
+- Prüfen, ob wir die Zelte am angegebenen Datum liefern können
+- Kontakt mit dem Kunden aufnehmen
+- Persönliche Offerte erstellen
+
+Gesendet mit ♥ von der Kultagentur-Hauta
+    ";
+    $headers = array('From:max.muster@gmail.com');
+
+    wp_mail( $to, $subject, $message, $headers );
+    wp_die();
+    
+}
 
 add_action( 'wp_ajax_nopriv_kah_get_data', 'kah_get_data' );
 
